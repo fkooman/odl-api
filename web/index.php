a2295cf185028e512f8aaf03be6948360b3e6899
François Kooman <fkooman@tuxed.net>
2015-10-30 10:52:22 +0100

    better error display

--- web/index.php
+++ web/index.php
@@ -10,79 +10,85 @@ use GuzzleHttp\Client;
 use fkooman\ODL\ApiCall;
 use fkooman\Http\RedirectResponse;
 
-$request = new Request($_SERVER);
+try {
+    $request = new Request($_SERVER);
 
-// config
-$iniReader = IniReader::fromFile(
-    dirname(__DIR__).'/config/config.ini'
-);
-
-$baseUrl = $iniReader->v('Api', 'baseUrl');
-
-// templates
-$templateManager = new TwigTemplateManager(
-    array(
-        dirname(__DIR__).'/views',
-        dirname(__DIR__).'/config/views',
-    ),
-    null
-);
-$templateManager->setDefault(
-    array(
-        'rootFolder' => $request->getUrl()->getRoot(),
-    )
-);
-
-$dataDir = dirname(__DIR__).'/data';
-
-// API call
-$client = new Client();
-$authUser = $iniReader->v('Api', 'authUser');
-$authPass = $iniReader->v('Api', 'authPass');
-$apiCall = new ApiCall($client, $dataDir, $authUser, $authPass);
-
-// supported flow names
-$supportedFlows = array();
-foreach (glob($dataDir.'/*', GLOB_ONLYDIR) as $dirName) {
-    $flowName = basename($dirName);
-    $supportedFlows[] = array(
-        'id' => $flowName,
-        'name' => ucfirst($flowName),
+    // config
+    $iniReader = IniReader::fromFile(
+        dirname(__DIR__).'/config/config.ini'
     );
-}
 
-// REST service
-$service = new Service();
+    $baseUrl = $iniReader->v('Api', 'baseUrl');
 
-// GET 
-$service->get(
-    '/',
-    function (Request $request) use ($templateManager, $supportedFlows) {
-        return $templateManager->render(
-            'index',
-            array(
-                'supportedFlows' => $supportedFlows,
-                'active' => $request->getUrl()->getQueryParameter('active'),
-            )
+    // templates
+    $templateManager = new TwigTemplateManager(
+        array(
+            dirname(__DIR__).'/views',
+            dirname(__DIR__).'/config/views',
+        ),
+        null
+    );
+    $templateManager->setDefault(
+        array(
+            'rootFolder' => $request->getUrl()->getRoot(),
+        )
+    );
+
+    $dataDir = dirname(__DIR__).'/data';
+
+    // API call
+    $client = new Client();
+    $authUser = $iniReader->v('Api', 'authUser');
+    $authPass = $iniReader->v('Api', 'authPass');
+    $apiCall = new ApiCall($client, $dataDir, $authUser, $authPass);
+
+    // supported flow names
+    $supportedFlows = array();
+    foreach (glob($dataDir.'/*', GLOB_ONLYDIR) as $dirName) {
+        $flowName = basename($dirName);
+        $supportedFlows[] = array(
+            'id' => $flowName,
+            'name' => ucfirst($flowName),
         );
     }
-);
 
-// POST
-$service->post(
-    '/',
-    function (Request $request) use ($templateManager, $apiCall, $baseUrl) {
-        // determine the flow to activate
-        $flowName = $request->getPostParameter('flow');
+    // REST service
+    $service = new Service();
 
-        $output = $apiCall->activate($baseUrl, $flowName);
+    // GET 
+    $service->get(
+        '/',
+        function (Request $request) use ($templateManager, $supportedFlows) {
+            return $templateManager->render(
+                'index',
+                array(
+                    'supportedFlows' => $supportedFlows,
+                    'active' => $request->getUrl()->getQueryParameter('active'),
+                )
+            );
+        }
+    );
 
-        return $output;
-#        return new RedirectResponse(
-#            $request->getUrl()->getRoot().sprintf('?active=%s', $flowName), 
-#            302
-#        );
-    }
-);
+    // POST
+    $service->post(
+        '/',
+        function (Request $request) use ($templateManager, $apiCall, $baseUrl) {
+            // determine the flow to activate
+            $flowName = $request->getPostParameter('flow');
 
-$service->run($request)->send();
+            $output = $apiCall->activate($baseUrl, $flowName);
+
+            return $output;
+    #        return new RedirectResponse(
+    #            $request->getUrl()->getRoot().sprintf('?active=%s', $flowName), 
+    #            302
+    #        );
+        }
+    );
+
+    $service->run($request)->send();
+} catch (Exception $e) {
+    echo '<pre>'.$e->getMessage().'</pre>';
+    echo '<hr>';
+    echo '<pre>'.$e->getTraceAsString().'</pre>';
+}
